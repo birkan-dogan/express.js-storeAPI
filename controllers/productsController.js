413b9d43b5b970b6389f177250d38e4f13d6a88c
birkan-dogan <b.561birkan@gmail.com>
2023-05-18 13:43:21 +0300

    working with req.query object, and sorting, select processes ✅

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,14 +6,38 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort, fields } = req.query; // that's our logic.
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+    // https://www.mongodb.com/docs/manual/reference/operator/query/regex/#x-and-s-options
+  }
+
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+
   res.status(200).json({ status: "success", size: products.length, products });
 };
 
